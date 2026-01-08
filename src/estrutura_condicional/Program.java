4acbcfccd586ee353938dd7e4c7132ce6f9335d0
Luiz Pizzi <luizcpizzi@gmail.com>
2026-01-07 22:03:39 -0300

    Aula estrutura condicional

---
+++ src/estrutura_condicional/Program.java
@@ -0,0 +1,28 @@
+package estrutura_condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double nota1 = sc.nextDouble();
+		double nota2 = sc.nextDouble();
+
+		double notaFinal = nota1 + nota2;
+
+		System.out.printf("NOTA FINAL = %.2f%n", notaFinal);
+
+		if (notaFinal < 60.0) {
+			System.out.println("REPROVADO");
+		}
+
+		sc.close();
+
+	}
+
+}
