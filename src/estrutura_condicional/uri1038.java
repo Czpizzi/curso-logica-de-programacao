15566b8126900657226a27ee2c6edfff912a99ec
Luiz Pizzi <luizcpizzi@gmail.com>
2026-01-12 20:44:42 -0300

    Novos exercicios de estrutura condicional

---
+++ src/estrutura_condicional/uri1038.java
@@ -0,0 +1,34 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class uri1038 {
+
+	public static void main(String[] args) {
+
+		        Scanner sc = new Scanner(System.in);
+
+		        int codigo = sc.nextInt();
+		        int quantidade = sc.nextInt();
+
+		        double preco = 0.0;
+
+		        if (codigo == 1) {
+		            preco = 4.00;
+		        } else if (codigo == 2) {
+		            preco = 4.50;
+		        } else if (codigo == 3) {
+		            preco = 5.00;
+		        } else if (codigo == 4) {
+		            preco = 2.00;
+		        } else if (codigo == 5) {
+		            preco = 1.50;
+		        }
+
+		        double total = preco * quantidade;
+
+		        System.out.printf("Total: R$ %.2f%n", total);
+
+		        sc.close();
+		    }
+		}
